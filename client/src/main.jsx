dfc25b093026e6f193869b94028b7a135c46bc3c
anaskhan28 <khananasatique@gmail.com>
2022-10-24 08:42:07 +0530

    revert the changes back

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,11 +7,11 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
   <TransactionProvider>
     <App />
   </TransactionProvider>
-  </HashRouter>,
+  </BrowserRouter>,
 );
 
 
