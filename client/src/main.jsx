580703caac932dfa8109611e4135a62764208b16
anaskhan28 <khananasatique@gmail.com>
2022-10-24 08:40:48 +0530

    used hash router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,11 +7,11 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
   <TransactionProvider>
     <App />
   </TransactionProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
 
 
